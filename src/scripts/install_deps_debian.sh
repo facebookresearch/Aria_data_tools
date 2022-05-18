47fc1d4214852294567301b758f618a860775dda
Pierre Moulon <pierrem@fb.com>
2022-05-18 15:35:41 -0700

    Add a build story for debian system (#1)
    Summary:
    GitHub workflow:
    - Add sudo
    - Since Podman is sudoless by default so we need to create a fake sudo binary in the Dockerfile
    
    Speed up 3rd party libs build
    
    Pull Request resolved: https://github.com/facebookresearch/Aria_data_tools/pull/1
    
    Reviewed By: fskarakas
    
    Differential Revision: D36469555
    
    fbshipit-source-id: 26ca17ab8a4a299a1f6c8c3925aab8fe0db41787

---
+++ src/scripts/install_deps_debian.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Install system deps
+# Toolchains
+sudo apt-get install -y cmake ninja-build ccache npm doxygen git
+# VRS, Sophus and Pybind11 dependencies
+sudo apt-get install -y \
+    libjpeg-dev libeigen3-dev \
+    libpng-dev liblz4-dev libzstd-dev libxxhash-dev \
+    libboost-system-dev libboost-filesystem-dev libboost-thread-dev libboost-chrono-dev libboost-date-time-dev \
+    libgtest-dev libgmock-dev libgoogle-glog-dev \
+    libglew-dev libgl1-mesa-dev \
+    libwayland-dev libxkbcommon-dev wayland-protocols \
+    libpython3-dev python3-pip;
+sudo apt-get clean;
+# Installing pybind11
+sudo pip3 install pybind11 numpy pyopengl;
+
+# Install and compile libraries
+sh ./install_compile_3rd_party.sh
