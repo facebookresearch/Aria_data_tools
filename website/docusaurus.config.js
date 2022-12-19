2f2b700a17078c87201a84a37d2395d5193b1794
Elizabeth Argall <ezar@meta.com>
2022-12-19 12:11:10 -0800

    Aria Data Tools MPS Update
    Summary: New MPS document and two supporting images.
    
    Reviewed By: vijay609
    
    Differential Revision: D42114269
    
    fbshipit-source-id: bf842811f62b5b930bd056d32ad4f6606aa9d16a

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,5 +1,17 @@
 /**
- * (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
@@ -16,7 +28,7 @@ const repoUrl = 'https://github.com/facebookresearch/Aria_data_tools';
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
   trailingSlash: true,
-  favicon: 'img/fav_aria_icon.png',
+  favicon: 'img/glasses-solid.svg',
   organizationName: 'facebook',
   projectName: 'aria-research-kit-open-data-tools',
 
