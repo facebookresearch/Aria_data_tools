551ad9d2375b40e5c538aa36d50b8e8f4ee16231
Pierre Moulon <pierrem@meta.com>
2023-03-10 17:34:02 -0800

    {VIZ} Adding EyeGaze on trajectory
    Summary: Enable to see Eye Gaze Vector along the trajectory
    
    Reviewed By: vijay609
    
    Differential Revision: D42761398
    
    fbshipit-source-id: 63f8e7952e82c1d72b619d8e067c501c1a983b0a

--- src/data_provider/mps_visualization/main_trajectory.cpp
+++ src/data_provider/mps_visualization/main_trajectory.cpp
@@ -16,9 +16,13 @@
 
 #include <pangolin/display/display.h>
 #include <pangolin/display/view.h>
+#include <pangolin/gl/gldraw.h>
 #include <pangolin/scene/axis.h>
 #include <pangolin/scene/scenehandler.h>
+#include <sophus/types.hpp>
+#include <algorithm>
 
+#include "eyeGazeReader.h"
 #include "trajectoryReader.h"
 
 using namespace ark::datatools;
@@ -29,26 +33,68 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
-  // Read the camera trajectory
-  // - It can be an open or close loop trajectory file.
-  const std::string locationFilePath = argv[1];
+  // Data IO
   Trajectory loadedPoses;
-  if (locationFilePath.find("open_loop_") != std::string::npos) {
-    std::cout << "Reading an Open Loop Trajectory file" << '\n';
-    loadedPoses = readOpenLoop(locationFilePath);
-  } else if (locationFilePath.find("closed_loop_") != std::string::npos) {
-    loadedPoses = readCloseLoop(locationFilePath);
-  } else {
-    std::cerr << "The filename does not contains the expected prefix 'open_loop_' or 'closed_loop'"
-              << std::endl;
-    return EXIT_FAILURE;
+  TemporalEyeGazeData eyeGazeRecords;
+  {
+    // Read the camera trajectory if any
+    {
+      // - It can be an open or close loop trajectory file.
+      const std::string locationFilePath = argv[1];
+
+      if (locationFilePath.find("open_loop_") != std::string::npos) {
+        std::cout << "Reading an Open Loop Trajectory file" << '\n';
+        loadedPoses = readOpenLoop(locationFilePath);
+      } else if (locationFilePath.find("closed_loop_") != std::string::npos) {
+        loadedPoses = readCloseLoop(locationFilePath);
+      } else {
+        std::cerr
+            << "The filename does not contains the expected prefix 'open_loop_' or 'closed_loop_'"
+            << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+
+    // Read Eye Gaze data record if any
+    eyeGazeRecords = (argc >= 3) ? readEyeGaze(argv[2]) : TemporalEyeGazeData();
   }
 
-  // Convert the pose to a format we can display with OpenGL
-  std::vector<Eigen::Vector3d> poses;
-  poses.reserve(loadedPoses.size());
-  for (const auto& pose : loadedPoses) {
-    poses.push_back(pose.T_world_device.translation());
+  // Convert data for 3D visualization
+  std::vector<Eigen::Vector3d> devicePoses;
+  std::vector<std::pair<Eigen::Vector3d, Eigen::Vector3d>> eyeGazeVectors;
+  {
+    // Convert the devicePoses to a format we can display with OpenGL
+    {
+      devicePoses.reserve(loadedPoses.size());
+      for (const auto& pose : loadedPoses) {
+        devicePoses.push_back(pose.T_world_device.translation());
+      }
+    }
+
+    // Convert read Eye Gaze data for 3D visualization
+    {
+      // For each data point:
+      //  - retrieve the closest pose
+      //  - build a line with the eye gaze ray (default assume 1 meter depth)
+      for (const auto& it : eyeGazeRecords) {
+        const auto eyeGazeTimestamp = it.first;
+        // Retrieve the closest pose
+        auto pose_info = std::lower_bound(
+            loadedPoses.begin(),
+            loadedPoses.end(),
+            eyeGazeTimestamp,
+            [](const TrajectoryPose& pose, std::chrono::microseconds value) {
+              return pose.tracking_timestamp_us < value;
+            });
+        if (pose_info != loadedPoses.end()) {
+          const Eigen::Vector3d center = pose_info->T_world_device.translation();
+          const Eigen::Vector3d gazeEndPoint =
+              center + pose_info->T_world_device.rotationMatrix() * it.second.gaze_vector;
+          // WIP: TBD Apply transform?
+          eyeGazeVectors.emplace_back(center, gazeEndPoint);
+        }
+      }
+    }
   }
 
   // Configure the Window and pangolin OpenGL Context for 3D drawing
@@ -75,9 +121,17 @@ int main(int argc, char** argv) {
     view.Activate(s_cam);
     tree.Render();
 
+    // Display device trajectory
     glColor3f(1, 0.8, 0);
     glLineWidth(3);
-    pangolin::glDrawLineStrip(poses);
+    pangolin::glDrawLineStrip(devicePoses);
+
+    // Display Eye Gaze vectors
+    glColor3f(0.4, 0.8, 1);
+    for (const auto& it : eyeGazeVectors) {
+      pangolin::glDrawLine(
+          it.first.x(), it.first.y(), it.first.z(), it.second.x(), it.second.y(), it.second.z());
+    }
   });
 
   while (!pangolin::ShouldQuit()) {
