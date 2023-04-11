2a2461129b3a385373ada0fe63807d142de0e5b8
Pierre Moulon <pierrem@meta.com>
2023-04-11 11:55:32 -0700

    {Build} Fix StreamIds definition for some compiler
    Summary:
    Behavior for extern variable was different for clang and gcc:
    - we are now defining them as static to simplify the definition of the variables for all compilers
    
    Reviewed By: vijay609
    
    Differential Revision: D44843026
    
    fbshipit-source-id: d0c4e87b368af3174f0713c5185be63a62601889

--- src/data_provider/AriaStreamIds.h
+++ src/data_provider/AriaStreamIds.h
@@ -20,17 +20,19 @@
 
 // List all StreamId related to Aria Data Layout
 namespace ark::datatools::dataprovider {
-extern const vrs::StreamId kEyeCameraStreamId;
-extern const vrs::StreamId kRgbCameraStreamId;
-extern const vrs::StreamId kSlamLeftCameraStreamId;
-extern const vrs::StreamId kSlamRightCameraStreamId;
-extern const vrs::StreamId kImuRightStreamId;
-extern const vrs::StreamId kImuLeftStreamId;
-extern const vrs::StreamId kMagnetometerStreamId;
-extern const vrs::StreamId kBarometerStreamId;
-extern const vrs::StreamId kAudioStreamId;
-extern const vrs::StreamId kWifiStreamId;
-extern const vrs::StreamId kBluetoothStreamId;
-extern const vrs::StreamId kGpsStreamId;
-extern const vrs::StreamId kTimeSyncStreamId;
+static const vrs::StreamId kEyeCameraStreamId{vrs::RecordableTypeId::EyeCameraRecordableClass, 1};
+static const vrs::StreamId kRgbCameraStreamId{vrs::RecordableTypeId::RgbCameraRecordableClass, 1};
+static const vrs::StreamId kSlamLeftCameraStreamId{vrs::RecordableTypeId::SlamCameraData, 1};
+static const vrs::StreamId kSlamRightCameraStreamId{vrs::RecordableTypeId::SlamCameraData, 2};
+static const vrs::StreamId kImuRightStreamId{vrs::RecordableTypeId::SlamImuData, 1};
+static const vrs::StreamId kImuLeftStreamId{vrs::RecordableTypeId::SlamImuData, 2};
+static const vrs::StreamId kMagnetometerStreamId{vrs::RecordableTypeId::SlamMagnetometerData, 1};
+static const vrs::StreamId kBarometerStreamId{vrs::RecordableTypeId::BarometerRecordableClass, 1};
+static const vrs::StreamId kAudioStreamId{vrs::RecordableTypeId::StereoAudioRecordableClass, 1};
+static const vrs::StreamId kWifiStreamId{vrs::RecordableTypeId::WifiBeaconRecordableClass, 1};
+static const vrs::StreamId kBluetoothStreamId{
+    vrs::RecordableTypeId::BluetoothBeaconRecordableClass,
+    1};
+static const vrs::StreamId kGpsStreamId{vrs::RecordableTypeId::GpsRecordableClass, 1};
+static const vrs::StreamId kTimeSyncStreamId{vrs::RecordableTypeId::TimeRecordableClass, 1};
 } // namespace ark::datatools::dataprovider
